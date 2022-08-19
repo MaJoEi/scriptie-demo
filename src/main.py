ccab8ab03257777ec77570ea2e32006a6c64ee13
Eisenlohr <matti.eisenlohr@tno.nl>
2022-08-19 14:00:07 +0200

    Added PyCharm project

---
+++ src/main.py
@@ -0,0 +1,36 @@
+import requests
+import json
+import wallet
+import verifier
+
+auth_url = "https://wallet.walt.id/api/auth/login"
+userinfo_url = "https://wallet.walt.id/api/auth/userInfo"
+didcreate_url = "https://custodian.ssikit.walt-test.cloud/did/create"
+
+auth = {
+  "id": "string",
+  "email": "string",
+  "password": "string",
+  "token": "string",
+  "ethAccount": "string",
+  "did": "string"
+}
+
+response = requests.post(auth_url, json=auth)
+resp = json.loads(response.text)
+token = resp['token']
+
+headers = {"Authorization": f"Bearer {token}"}
+
+userinfo_req = requests.get(userinfo_url, headers=headers)
+userinfo = json.loads(userinfo_req.text)
+
+print(userinfo)
+
+didcreate_body = {
+    "method": "key"
+}
+
+didresp = requests.post(didcreate_url, json=didcreate_body)
+print(didresp.text)
+
